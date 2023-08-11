55d59106ed1c75f9aa816596166b8926744501ed
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 10:09:05 -0700

    if structure

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,13 @@
 # include<stdio.h>
 /**
- *advanced task ! 
- *
- *
- *
- *
- *
- *
- * */
+*advanced task ! 
+*
+*
+*
+*
+*
+*
+**/
 int main (void)
 {
 	printf ("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
--- README.md
+++ README.md
@@ -1,1 +1,2 @@
  alx-low_level_programming
+Low programming in C about if conditionnal structure
