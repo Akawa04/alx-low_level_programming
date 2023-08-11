32adba449fa3ff795f1109bedb768bf0b41a54c0
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 16:09:55 -0700

    mandatory Clow programming

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/*
+ *
+ *
+ * Alphabet*/
+int main (void)
+{
+	char alphabet[] = { 'a' , 'b' , 'c' , 'd' , 'e' , 'f' , 'g' , 'h' , 'i' , 'j' , 'k' , 'l' , 'm' , 'n' , 'o' , 'p' , 'q' , 'r' , 's' , 't' , 'u' , 'v' , 'w' , 'x' , 'y' , 'z' };
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		if (alphabet[i] != 'e' && alphabet[i] != 'q' )
+		{
+			putchar(alphabet[i]);
+		}
+	}
+	putchar('\n');
+	return (0);
+}
