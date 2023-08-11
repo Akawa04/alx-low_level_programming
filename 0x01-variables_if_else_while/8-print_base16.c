5c50abae1c18e71fd1a3725b65e15da8ae1311c7
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 16:52:45 -0700

    mandatory Clow programming

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ *
+ *
+ * Alphabet*/
+int main (void)
+{
+	char alphabet[] = { 'a' ,'b' ,'c' ,'d' ,'e' ,'f'};
+	int i;
+	for (i = 0; 0 <= 15; i++)
+	{
+		putchar(alphabet[i]);
+	}
+	putchar('\n');
+	return (0);
+}
