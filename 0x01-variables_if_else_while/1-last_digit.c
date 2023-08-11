a44ad7731301fa7a7a4a0e23cccf5dd7a9faabef
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 15:00:06 -0700

    mandatory Clow programming

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+int main(void)
+{
+	int n, rest;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	rest = n%10;
+	if (rest > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, rest);
+	}
+	else if (rest == 0)
+	{
+		printf("Last digit of %d is %d and is 0\n", n, rest);
+	}
+	else
+	{
+		printf("Last digit of %d is %d and is less than 6 and not 0\n", n, rest);
+	}
+	return (0);
+}
