0dda151c7b6d264e78a18cbafdfc168f2b9c4f6f
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 15:50:14 -0700

    mandatory Clow programming

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/*
+ *
+ *
+ * Alphabet*/
+int main (void)
+{
+	char alphabet[] = { 'a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t', 'u', 'v' ,'w' ,'x' ,'y' , 'z' }, ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	int i;
+	for (i = 0; i < 26; i++)
+	{
+		putchar(alphabet[i]);
+	}
+
+	for (i = 0; i < 26; i++)
+        {
+                putchar(ALPHABET[i]);
+        }
+
+	putchar('\n');
+	return (0);
+}
