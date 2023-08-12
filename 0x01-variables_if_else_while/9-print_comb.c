723552a07e7e8dacfa96696841954d37f699e812
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 17:20:35 -0700

    mandatory Clow programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,12 +6,16 @@
  * Alphabet*/
 int main (void)
 {
-	char alphabet[] = { '0' , ',' , ' ' , '1', ',' , ' ' , '2' , ',' , ' ' , '3', ',' , ' ' , '4', ',' , ' ' , '5', ',' , ' ' , '6', ',' , ' ' , '7', ',' , ' ' , '8', ',' , ' ' , '9' };
+	string alphabet[] = { '0', '1','2' , '3','4', '5' , '6',  '7',  '8', '9' };
 	int i;
-	for (i = 0; i < 31; i++)
+	do
 	{
 		putchar(alphabet[i]);
+		putchar(',');
+		putchar(' ');
+		i++;
 	}
+	while (i < 10)
 	putchar('\n');
 	return (0);
 }
