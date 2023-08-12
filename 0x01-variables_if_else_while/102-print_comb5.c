d8c93484d19cfd0f693578bfba090d30b0761de4
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 09:25:25 -0700

    betty correction

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+/**
+ * main - Entry parameters
+ * Description:This program prints all possible
+ * different combinations of two digits.
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int a, b, c;
+
+	for (a = 0; a < 10; a++)
+	{
+		for (b = 0; b < 10; b++)
+		{
+			for (c = 0; c < 10; c++)
+			{
+				putchar('0' + a);
+				putchar('0' + b);
+				if (a != 7 || b != 8)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
