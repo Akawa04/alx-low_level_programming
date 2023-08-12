d6f12434f1fcc6fb0a96e4e6569ece7284389e61
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 08:58:53 -0700

    betty correction

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -1,14 +1,15 @@
 #include <stdio.h>
 /**
  * main - Entry parameters
- * Description:This program prints all possible different combinations of two digits.
+ * Description:This program prints all possible
+ * different combinations of two digits.
  * Return: Always 0 (success)
  */
 
 int main(void)
 {
 	int a, b;
-	
+
 	for (a = 0; a < 10; a++)
 	{
 		for (b = 0; b < 10; b++)
