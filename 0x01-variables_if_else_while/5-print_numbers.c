279d2dadd396b5d8dc618726fd53e93bfcd48825
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:13:42 -0700

    betty correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,12 +10,11 @@
 
 int main(void)
 {
-	char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	int i;
 
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		printf("%c", num[i]);
+		printf("%c", i);
 	}
 	return (0);
 }
