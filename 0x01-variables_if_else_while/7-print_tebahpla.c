fb4a418676eb53cc130b4c07118614c679224051
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:42:59 -0700

    betty correction

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -3,19 +3,19 @@
 /**
  * main - Entry point
  *
- * Description: This program prints the digits from 0 to 9.
+ * Description: This program display alphabet in reverse..
  *
  * Return: Always 0 (Success)
  */
 
 int main(void)
 {
-	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
-			, 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i',
+			'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
 			'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
 
-	for (i = 25; 0 <= i; i--)
+	for (i = 25; i >= 0; i--)
 	{
 		putchar(alphabet[i]);
 	}
