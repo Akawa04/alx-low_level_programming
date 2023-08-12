43d4468a3f2c3c23eeef0e0460d057091b17d2f6
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:06:09 -0700

    betty correction

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
  *
+ * Description: This program prints the digits from 0 to 9.
  *
- * Alphabet*/
+ * Return: Always 0 (Success)
+ */
 int main (void)
 {
 	char num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	int i;
+
 	for (i = 0; i < 11; i++)
 	{
-		putchar(num[i]);
+		printf("%c", num[i]);
 	}
-	putchar('\n');
+	printf("\n");
 	return (0);
 }
