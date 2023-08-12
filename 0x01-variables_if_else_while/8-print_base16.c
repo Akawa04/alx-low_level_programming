c5ec58b15a614e537d50aebe7d0868d906a735f0
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:48:45 -0700

    betty correction

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 
-/*
- *
- *
- * Alphabet*/
-int main (void)
+/**
+ * main 
+ * hexadecimal caracters display. 
+ * Return: Always 0 is success.
+ */
+int main(void)
 {
-	char alphabet[] = {'0', '1','2','3','4','5','6','7','8','9','a' ,'b' ,'c' ,'d' ,'e' ,'f'};
+	char alphabet[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f'};
 	int i;
 	for (i = 0; i <= 15; i++)
 	{
