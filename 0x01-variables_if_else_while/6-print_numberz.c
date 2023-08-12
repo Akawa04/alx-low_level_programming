5b8e111c0e155e014e98b49ff5e9c02a6780d2e7
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:25:11 -0700

    betty correction

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,16 +1,16 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
  *
- *
- * Alphabet*/
-int main (void)
+ * Return: Always 0 success
+ */
+int main(void)
 {
-	int num[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'};
 	int i;
-	for (i = 0; i < 11; i++)
+	for (i = 0; i < 10; i++)
 	{
-		putchar(num[i]);
+		putchar(i);
 	}
 	putchar('\n');
 	return (0);
--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,12 +1,18 @@
 #include <stdio.h>
 
-/*
+/**
+ * main - Entry point
  *
+ * Description: This program prints the digits from 0 to 9.
  *
- * Alphabet*/
-int main (void)
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
 {
-	char alphabet[] = { 'a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t', 'u', 'v' ,'w' ,'x' ,'y' , 'z' };
+	char alphabet[] = {'a','b','c','d','e','f','g','h','i'
+			,'j','k','l','m','n','o','p','q','r','s','t', 
+			'u', 'v','w','x','y', 'z'};
 	int i;
 	for (i = 25; 0 <= i; i--)
 	{
