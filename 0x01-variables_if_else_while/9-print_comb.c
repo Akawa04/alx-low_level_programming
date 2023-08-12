0eafe051a2fa075a45d23baed9d86bc5b6932de9
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 04:14:11 -0700

    betty correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -9,8 +9,9 @@
 
 int main(void)
 {
-	int alphabet[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},i = 0;
-
+	int alphabet[] = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9'},
+	i = 0;
+	
 	do
 	{
 		putchar(alphabet[i]);
