b0c11e6f1ac4306ab8c61e5cabc005806e1cd1de
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:39:09 -0700

    betty correction

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -10,10 +10,11 @@
 
 int main(void)
 {
-	char alphabet[] = {'a','b','c','d','e','f','g','h','i'
-			,'j','k','l','m','n','o','p','q','r','s','t', 
-			'u', 'v','w','x','y', 'z'};
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i'
+			, 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 
+			'u', 'v', 'w', 'x', 'y', 'z'};
 	int i;
+
 	for (i = 25; 0 <= i; i--)
 	{
 		putchar(alphabet[i]);
