e09df9b3d82f97ea4efd206bb7b8d8a5c56876b2
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 02:56:31 -0700

    betty correction

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,13 +1,22 @@
 #include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Description: This program prints the lowercase and uppercase alphabets.
+ *
+ * Return: Always 0 (Success)
+ */
 
-/*
- *
- *
- * Alphabet*/
-int main (void)
+int main(void)
 {
-	char alphabet[] = { 'a' ,'b' ,'c' ,'d' ,'e' ,'f' ,'g' ,'h' ,'i' ,'j' ,'k' ,'l' ,'m' ,'n' ,'o' ,'p' ,'q' ,'r' ,'s' ,'t', 'u', 'v' ,'w' ,'x' ,'y' , 'z' }, ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'};
+	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
+			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u', 
+			'v', 'w', 'x', 'y', 'z'}, 
+	     ALPHABET[] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 
+			'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 
+			'V', 'W', 'X', 'Y', 'Z'};
 	int i;
+
 	for (i = 0; i < 26; i++)
 	{
 		putchar(alphabet[i]);
