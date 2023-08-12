91b723b2b349387c7b6b61410ea9fdc01900f847
Akawa04 <lekassalouis13@gmail.com>
2023-08-11 17:21:27 -0700

    mandatory Clow programming

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -6,7 +6,7 @@
  * Alphabet*/
 int main (void)
 {
-	string alphabet[] = { '0', '1','2' , '3','4', '5' , '6',  '7',  '8', '9' };
+	int alphabet[] = { '0', '1','2' , '3','4', '5' , '6',  '7',  '8', '9' };
 	int i;
 	do
 	{
