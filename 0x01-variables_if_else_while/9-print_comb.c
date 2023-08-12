900bbdca464209d53d93bb8b0f2f68a2761f69b4
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 04:23:15 -0700

    betty correction

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -2,7 +2,8 @@
 
 /**
  * main - Entry point
- * Description: This program prints the digits from 0 to 9 separated by commas and spaces.
+ * Description: This program prints the digits from 0 
+ * to 9 separated by commas and spaces.
  * Return: Always 0 (Success)
  */
 
