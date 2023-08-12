52dfb6518a8f1a71c65b6bc5bd8cdb8e0d75b6ad
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 03:01:09 -0700

    betty correction

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -10,7 +10,7 @@ int main (void)
 {
 	char alphabet[] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j',
 			'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't','u',
-			'v', 'w', 'x', 'y', 'z'},
+			'v', 'w', 'x', 'y', 'z'};
 	int i;
 
 	for (i = 0; i < 26; i++)
