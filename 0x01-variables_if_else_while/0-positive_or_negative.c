e41503e9544a14725968695b1c5f4cd3c3c12845
Akawa04 <lekassalouis13@gmail.com>
2023-08-12 02:24:49 -0700

    betty correction

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -3,14 +3,21 @@
 #include <stdio.h>
 /* more headers goes there */
 
-/* betty style doc for function main goes there */
+/**
+ * main - Entry point
+ *
+ * This is a simple program to determine whether a random number is
+ * positive, negative, or zero.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	int n;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	/* your code goes there */
+
 	if (n == 0)
 	{
 		printf("%d is zero\n", n);
@@ -25,3 +32,4 @@ int main(void)
 	}
 	return (0);
 }
+
