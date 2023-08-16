a4565552380eaabec5615ac6a07e8a955716d3dd
Akawa04 <lekassalouis13@gmail.com>
2023-08-16 14:27:34 -0700

    nestd loop

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,17 +1,22 @@
 #include <stdio.h>
 #include "main.h"
 /**
+ * main - Entry point of the program
  *
- * 
+ * Return: Always 0 (success)
  */
-
 int main(void)
 {
 	laff();
-	return(0);
+	return (0);
 }
 
-void laff()
+/**
+ * laff - Function to print "_putchar" followed by a newline
+ *
+ * Return: None
+ */
+void laff(void)
 {
 	printf("_putchar\n");
 }
