2c0ddab94262720cf09308b9087dcb87acdb2805
Akawa04 <lekassalouis13@gmail.com>
2023-08-18 01:33:39 -0700

    Function order by header FILE

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,19 +1,22 @@
 #include "main.h"
-
+/**
+ * print_diagonal - Draws a diagonal line using '\'
+ * @n: The number of times the character '\' should be printed
+ */
 void print_diagonal(int n)
 {
 	int i,j;
+
 	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
 		{
+			_putchar('\\');
+			_putchar('\n');
 			for (j = 1; j < i; j++)
-
 			{
 				_putchar(' ');
 			}
-			_putchar('\\');
-			_putchar('\n');
 		}
 	}
 	else
