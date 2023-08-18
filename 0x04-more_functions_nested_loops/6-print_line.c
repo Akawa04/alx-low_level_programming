8351b105d26e33c73d9728b1c66fd12fcf908d6c
Akawa04 <lekassalouis13@gmail.com>
2023-08-18 01:18:49 -0700

    Function order by header FILE

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,8 +1,14 @@
 #include "main.h"
 
+/**
+ * print_line - Prints a line of underscores
+ * @n: The number of underscores to print
+ */
+
 void print_line(int n)
 {
 	int i;
+
 	if (n > 0)
 	{
 		for (i = 1; i <= n; i++)
