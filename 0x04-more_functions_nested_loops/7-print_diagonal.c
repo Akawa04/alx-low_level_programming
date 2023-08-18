a9f2728f5f47f9d05b6ddc881a6e6c239a79090b
Akawa04 <lekassalouis13@gmail.com>
2023-08-18 00:53:03 -0700

    Function order by header FILE

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -1,15 +1,19 @@
 #include "main.h"
 
-void print_line(int n)
+void print_diagonal(int n)
 {
-	int i;
+	int i,j;
 	if (n > 0)
 	{
-		for (i = 0; i <= n; i++)
+		for (i = 1; i <= n; i++)
 		{
-			_putchar('_');
+			_putchar('\\');
+			_putchar('\n');
+			for (j = 1; j < i; j++)
+			{
+				_putchar(' ');
+			}
 		}
-		_putchar('\n');
 	}
 	else
 	{
