9e369fbca29fe963a85e331c5c46525c272cda78
Akawa04 <lekassalouis13@gmail.com>
2023-08-18 01:09:57 -0700

    Function order by header FILE

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,8 +1,12 @@
 #include "main.h"
+/**
+ * more_numbers - Prints numbers from 0 to 14 ten times
+ */
 
 void more_numbers(void)
 {
 	int num,fois;
+
 	for (fois = 1; fois <= 10; fois++)
 	{
 		for(num = 0; num <= 9; num++)
@@ -11,7 +15,7 @@ void more_numbers(void)
 		}
 		for(num = 0; num <= 4; num++)
                 {
-			_putchar('1');
+			_putchar('0' + 1);
                         _putchar('0' + num);
                 }
 		_putchar('\n');
